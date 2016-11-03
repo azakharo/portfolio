37f03c8157c84926fb7d55a1ac089e743394e7fe
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-03 13:40:37 +0300

    Provided smooth scrolling for nav items.

--- client/app/main/layout.js
+++ client/app/main/layout.js
@@ -40,13 +40,13 @@ function MyLayout() {
     $offset = $(".navbar-fixed-top").height() - 20;
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    //$('.js_nav-item a').bind('click', function (event) {
-    //  var $position = $($(this).attr('href')).offset().top;
-    //  $('html, body').stop().animate({
-    //    scrollTop: $position - $offset
-    //  }, 600);
-    //  event.preventDefault();
-    //});
+    $('.js_nav-item a').bind('click', function (event) {
+      var $position = $($(this).attr('href')).offset().top;
+      $('html, body').stop().animate({
+        scrollTop: $position - $offset
+      }, 600);
+      event.preventDefault();
+    });
 
     var $scrollspy = $('body').scrollspy({target: '.navbar-fixed-top', offset: $offset + 2});
 
