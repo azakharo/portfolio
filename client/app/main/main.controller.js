d38821492cb9678190ebe8082a837a736d66e194
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-03 17:42:16 +0300

    Impl-ed back-to-top.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,5 +13,39 @@ angular.module('projectsApp')
       layout = MyLayout();
       layout.init();
       $('.parallax-window').parallax({imageSrc: 'assets/images/1920x1080/01.jpg'});
+
+
+      ///////////////////////////////////////////////////////////////
+      // Back to top
+
+      // browser window scroll (in pixels) after which the "back to top" link is shown
+      var offset = 300,
+      //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+      //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+      //grab the "back to top" link
+        $back_to_top = $('.js-back-to-top');
+
+      //hide or show the "back to top" link
+      $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('back-to-top-is-visible') : $back_to_top.removeClass('back-to-top-is-visible back-to-top-fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+          $back_to_top.addClass('back-to-top-fade-out');
+        }
+      });
+
+      //smooth scroll to top
+      $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0
+          }, scroll_top_duration
+        );
+      });
+
+      // Back to top
+      ///////////////////////////////////////////////////////////////
+
     }, 100);
   });
