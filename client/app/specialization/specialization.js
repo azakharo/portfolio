1873246a389698517ec1291a67459b33ed31e798
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-11-23 18:04:11 +0300

    Specialization section is ready.

--- client/app/specialization/specialization.js
+++ client/app/specialization/specialization.js
@@ -2,6 +2,9 @@
 
 angular.module('projectsApp')
   .component('specialization', {
-    templateUrl: 'app/specialization/specialization.html'
+    templateUrl: 'app/specialization/specialization.html',
+    controller: 'SpecializationCtrl'
+  })
+  .controller('SpecializationCtrl', function ($scope, MyConstants) {
+    $scope.resumeUrl = MyConstants.resumeUrl;
   });
-
